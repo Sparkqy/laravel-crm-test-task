84fbbe0945289417e26c4b80d8c67b4479bd3913
Illia Koliadenko <triiko21@gmail.com>
2020-07-08 13:27:24 +0300

    Tweaked default LoginController to work with API; Configured .env.example

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,37 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request)) {
+            if ($request->wantsJson()) {
+                $user = $this->guard()->user();
+                $user->generateToken();
+
+                return response()->json(['data' => $user->toArray()]);
+            }
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * @return string
+     */
+    public function username()
+    {
+        return 'login';
     }
 }
