b6eb402ea9ef81be2eec1815c0143b937912bfa1
Illia Koliadenko <triiko21@gmail.com>
2020-07-08 16:23:01 +0300

    Added ON DELETE action for foreign keys of client_company table

---
+++ src/database/migrations/2020_07_08_131800_add_column_to_client_company_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToClientCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client_company', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+            $table->dropForeign(['company_id']);
+
+            $table->foreign('client_id')
+                ->references('id')
+                ->on('clients')
+                ->onDelete('cascade');
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('client_company', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+            $table->dropForeign(['company_id']);
+        });
+    }
+}
